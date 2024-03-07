da0f7542369a9fcfafa27eceab21c0763913991b
Taufeeq <taufeeq.riyaz64@gmail.com>
2024-03-07 11:44:42 +0530

    Update ex-1.py

--- ex-1.py
+++ ex-1.py
@@ -18,7 +18,6 @@ print(f"The last 3 elements of tuple D are {d[-3:]}")
 print(f"The length of tuple D is {len(d)}")
 
 # In[5]:
- #read marks of n students in a course as a tuple and find the sum average and higest and lowest
 n = int(input("Enter the number of students - "))
 marks = []
 for i in range(n):
@@ -29,3 +28,5 @@ print(f"The sum of marks of students is {sum(marks)}")
 print(f"The average of marks of students is {sum(marks)/n}")
 print(f"The highest marks of students is {max(marks)}")
 print(f"The lowest marks of students is {min(marks)}")
+
+# In[6]
